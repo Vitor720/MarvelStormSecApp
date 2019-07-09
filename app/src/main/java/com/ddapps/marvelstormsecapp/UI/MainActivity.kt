8edeedb817369307aaec636b1e88ae4c63807847
Vitor720 <nun720@gmail.com>
2019-07-09 17:38:58 -0300

    Initial commit

---
+++ app/src/main/java/com/ddapps/marvelstormsecapp/UI/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ddapps.marvelstormsecapp.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ddapps.marvelstormsecapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
